416ecb89f5cd09c2f70c31481b68cdfbdff19fb6
Jorge González Bravo <JorgeGBravo@users.noreply.github.com>
2021-05-31 21:06:44 +0100

    Implementacion de correo admincomicsypeliculas@comicsypeliculas.com.

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img class="w-52 h-52 fill-current text-gray-500" src="{{asset("storage/MARVEL & The Movie db-grey.png")}}" />
             </a>
         </x-slot>
 
